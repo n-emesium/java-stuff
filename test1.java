fbec7e02dd9c38b6122368ffdae2ab8e1f54a666
nemesis <78811138+n-emesium@users.noreply.github.com>
2024-01-31 04:51:03 +0300

    Update test1.java
    updated formatting

--- test1.java
+++ test1.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 //class definition under static context; cannot access method under subclass;
 //directly use fperm() method to generate instead of creating an instance;
 //works with recursion, try not to force too many stack calls; otherwise the program will not exit();
-public class test1 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> results = new ArrayList<String>();
@@ -14,16 +14,16 @@ public class test1 {
         fperm(results, new ArrayList<Character>(), f);
         printlst(results);
     }
-    public static void fperm(ArrayList<String> results ,ArrayList<Character> current, String f) {
-        if (f.length() == 0 && !(results.contains(strconv(current)))) {
+    public static void fperm(ArrayList<String> results, ArrayList<Character> current, String f) {
+        if (!(results.contains(strconv(current))) && f.length() == 0) {
             results.add(strconv(current));
             return;
         }
         for (int i = 0; i < f.length(); i++) {
-            current.add(f.charAt(i));
-            String fPrime = f.substring(0,i) + f.substring(i+1);
-            fperm(results, current, fPrime);
-            current.remove(current.size()-1);
+            current.add(f.charAt(i)); //the character that has been added;
+            String fp = f.substring(0,i) + f.substring(i+1); //this character will be discarded;
+            fperm(results, current,fp);
+            current.remove(current.size() - 1); //backtracked;
         }
     }
     public static String strconv(ArrayList<Character> carr) {
@@ -33,41 +33,16 @@ public class test1 {
         }
         return m;
     }
-    /*
-    public static boolean compare(ArrayList<Character> carr, String f) {
-        for (int i = 0; i < f.length(); i++) {
-            if (carr.get(i) != f.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    */
     public static void printlst(ArrayList<String> results) {
         System.out.println();
+        int d = 0;
         for (int i = 0; i < results.size(); i++) {
-            System.out.print(results.get(i) + " ");
-        }
-        System.out.println();
-        System.out.println();
-    }
-    //use method to check if all characters are present;
-    /*public static boolean allcntn(String f, ArrayList<Character> carr) {
-        String con = strconv(carr);
-        for (int i = 0; i < con.length(); i++) {
-            if (f.indexOf(con.charAt(i)) == -1) {
-                return false;
+            if (d % 6 == 0) {
+                System.out.println();
             }
+            System.out.print(results.get(i) + " ");
+            d++;
         }
-        return true;
+        System.out.println();
     }
-    */
-    /*
-    private static void flush(ArrayList<Character> carr) {
-        int i = 0;
-        while (i != carr.size()) {
-            carr.remove(i);
-        }
-    }
-    */
 }
